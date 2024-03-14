cb950154e03062e1c74255da59b7ac0232235dd8
Sudeep <sdreple@gmail.com>
2024-03-14 12:29:47 +0545

    merged

--- flask-server/server.py
+++ flask-server/server.py
@@ -45,7 +45,7 @@ def generate_frames(process_every_nth_frame = 10):
 
                 print(detected_animal_names_list)
                 animal_names_json = json.dumps(detected_animal_names_list)
-                # send_detected_animals_list(detected_animal_names_list) # Send the detected animal names list to another endpoint
+                send_detected_animals_list(detected_animal_names_list) # Send the detected animal names list to another endpoint
 
                 ret, jpeg = cv2.imencode('.jpg', last_processed_frame)
                 jpeg_buffer = jpeg.tobytes()
@@ -55,7 +55,7 @@ def generate_frames(process_every_nth_frame = 10):
                 
             else:
                 if last_processed_frame is not None:
-                    # send_detected_animals_list(detected_animal_names_list) # Send the detected animal names list to another endpoint
+                    send_detected_animals_list(detected_animal_names_list) # Send the detected animal names list to another endpoint
                     animal_names_json = json.dumps(detected_animal_names_list)
                     ret, jpeg = cv2.imencode('.jpg', last_processed_frame)
                     jpeg_buffer = jpeg.tobytes()
